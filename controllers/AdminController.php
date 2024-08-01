d383c79af11e7dd3a5e09748911e103fb15cadde
maristefer <mirtilm.eva@gmail.com>
2024-08-01 22:34:20 +0000

    add fonction login-admin à tester

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -16,7 +16,51 @@ class AdminController extends AbstractController
         $this->render('admin/login.html.twig', []);
     }
 
-    public function checkLogin() : void {
+    public function checkLogin() : void 
+    {
+        if(isset($_POST["email"]) && isset($_POST["password"]))
+        {
+            $tokenManager = new CSRFTokenManager();
+
+            if(isset($_POST["csrf_token"]) && $tokenManager->validateCSRFToken($_POST["csrf_token"]))
+            {
+                $um = new UserManager();
+                $user = $um->findUserByEmail($_POST["email"]);
+
+                if($user !== null)
+                {
+                    if(password_verify($_POST["password"], $user->getPassword()) && $user->getRole() === "ADMIN")
+                    {
+                        $_SESSION["user"] = $user->getId();
+                        $_SESSION["role"] = $user->getRole();
+
+                        unset($_SESSION["error_message"]);
+
+                        $this->redirect("admin");
+                    }
+                    else
+                    {
+                        $_SESSION["error_message"] = "Invalid login information";
+                        $this->redirect("admin-connexion");
+                    }
+                }
+                else
+                {
+                    $_SESSION["error_message"] = "Invalid login information";
+                    $this->redirect("admin-connexion");
+                }
+            }
+            else
+            {
+                $_SESSION["error_message"] = "Invalid CSRF token";
+                $this->redirect("admin-connexion");
+            }
+        }
+        else
+        {
+            $_SESSION["error_message"] = "Missing fields";
+            $this->redirect("admin-connexion");
+        }
         
     }
 }
